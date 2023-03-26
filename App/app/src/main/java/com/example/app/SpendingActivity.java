aae905cfb87866b743b269b3eb8058b94fc79435
Ivanlcrv <ivanlcrv@gmail.com>
2023-03-26 13:09:08 +0200

    spending activity

--- App/app/src/main/java/com/example/app/SpendingActivity.java
+++ App/app/src/main/java/com/example/app/SpendingActivity.java
@@ -1,11 +1,9 @@
 package com.example.app;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.app.databinding.ActivitySpendingBinding;
@@ -14,21 +12,15 @@ import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
-import kotlin.Pair;
-
 public class SpendingActivity extends AppCompatActivity {
-    private ActivitySpendingBinding binding;
     private BarChart barChart;
     private BarData barData;
     private BarDataSet barDataSet;
@@ -38,7 +30,7 @@ public class SpendingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivitySpendingBinding.inflate(getLayoutInflater());
+        com.example.app.databinding.ActivitySpendingBinding binding = ActivitySpendingBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         myRef = FirebaseDatabase.getInstance().getReference();
@@ -48,35 +40,63 @@ public class SpendingActivity extends AppCompatActivity {
             getBarEntries(check.getText().toString());
         });
         barChart = binding.idBarChart;
+        barChart.setNoDataText("No expenses");
         getBarEntries("Daily");
+        barChart.getXAxis().setEnabled(false);
 
     }
     private void getBarEntries(String interval) {
         barEntriesArrayList = new ArrayList<>();
-        myRef.child("shopping").child(uid).child("expenses").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if(task.isSuccessful()){
-                    if(interval.equals("Daily")){
-                        float i = 1;
-                        for(DataSnapshot dataSnapshot : task.getResult().getChildren()){
-                            barEntriesArrayList.add(new BarEntry(i, dataSnapshot.getValue(Float.class)));
-                            i++;
-                        }
-
+        myRef.child("shopping").child(uid).child("expenses").get().addOnCompleteListener(task -> {
+            String name = "";
+            if(task.isSuccessful()){
+                if(interval.equals("Daily")){
+                    float pos_x = 1;
+                    for(DataSnapshot dataSnapshot : task.getResult().getChildren()){
+                        barEntriesArrayList.add(new BarEntry(pos_x, dataSnapshot.getValue(Float.class)));
+                        pos_x++;
                     }
-                    else if(interval.equals("Monthly")){
-                        float i = 1;
-                        for(DataSnapshot dataSnapshot : task.getResult().getChildren()){
-                            barEntriesArrayList.add(new BarEntry(i, dataSnapshot.getValue(Float.class)));
-                            i++;
-                        }
-                    }
-                    else{
-
-                    }
+                    name = "Daily expenses";
                 }
-                else Toast.makeText(SpendingActivity.this, "No expenses", Toast.LENGTH_SHORT).show();
+                else if(interval.equals("Monthly")){
+                    float pos_x = 1;
+                    float sum = 0;
+                    String month = Objects.requireNonNull(task.getResult().getChildren().iterator().next().getKey()).split("-")[1];
+                    for(DataSnapshot dataSnapshot : task.getResult().getChildren()){
+                        String[] date = Objects.requireNonNull(dataSnapshot.getKey()).split("-");
+                        if(month.equals(date[1])){
+                            sum += dataSnapshot.getValue(Float.class);
+                        }
+                        else{
+                            barEntriesArrayList.add(new BarEntry(pos_x, sum));
+                            pos_x++;
+                            sum = dataSnapshot.getValue(Float.class);
+                            month = dataSnapshot.getKey().split("-")[1];
+                        }
+                    }
+                    barEntriesArrayList.add(new BarEntry(pos_x, sum));
+                    name = "Monthly expenses";
+                }
+                else{
+                    float pos_x = 1;
+                    float sum = 0;
+                    String month = Objects.requireNonNull(task.getResult().getChildren().iterator().next().getKey()).split("-")[2];
+                    for(DataSnapshot dataSnapshot : task.getResult().getChildren()){
+                        String[] date = Objects.requireNonNull(dataSnapshot.getKey()).split("-");
+                        if(month.equals(date[2])){
+                            sum += dataSnapshot.getValue(Float.class);
+                        }
+                        else{
+                            barEntriesArrayList.add(new BarEntry(pos_x, sum));
+                            pos_x++;
+                            sum = dataSnapshot.getValue(Float.class);
+                            month = dataSnapshot.getKey().split("-")[2];
+                        }
+                    }
+                    barEntriesArrayList.add(new BarEntry(pos_x, sum));
+                    name = "Yearly expenses";
+                }
+
                 barDataSet = new BarDataSet(barEntriesArrayList, "Data user");
                 barData = new BarData(barDataSet);
                 barChart.setData(barData);
@@ -85,6 +105,15 @@ public class SpendingActivity extends AppCompatActivity {
                 barDataSet.setValueTextSize(16f);
                 barChart.getDescription().setEnabled(false);
             }
+            else Toast.makeText(SpendingActivity.this, "No expenses", Toast.LENGTH_SHORT).show();
+            barDataSet = new BarDataSet(barEntriesArrayList, name);
+            barData = new BarData(barDataSet);
+            barChart.setData(barData);
+            barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+            barDataSet.setValueTextColor(R.color.primary_blue);
+            barDataSet.setValueTextSize(16f);
+            barChart.getDescription().setEnabled(false);
+            barChart.invalidate();
         });
     }
 }
--- App/app/src/main/java/com/example/app/ui/shopping/ShoppingFragment.java
+++ App/app/src/main/java/com/example/app/ui/shopping/ShoppingFragment.java
@@ -125,7 +125,7 @@ public class ShoppingFragment extends Fragment {
                     Calendar calendar = Calendar.getInstance();
                     String date = calendar.get(Calendar.DAY_OF_MONTH) + "-" + (calendar.get(Calendar.MONTH) + 1) + "-" + calendar.get(Calendar.YEAR);
                     myRef.child("shopping").child(user.getUid()).child("expenses").child(date).get().addOnCompleteListener(task -> {
-                        if (task.isSuccessful())
+                        if (task.getResult().getValue() != null)
                             myRef.child("shopping").child(user.getUid()).child("expenses").child(date).setValue(Integer.parseInt(amount) + Integer.parseInt(Objects.requireNonNull(task.getResult().getValue(String.class))));
                         else
                             myRef.child("shopping").child(user.getUid()).child("expenses").child(date).setValue(amount);
